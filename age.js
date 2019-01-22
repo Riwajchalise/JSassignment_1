c237194c1fd50e9a9ff9f9126e16c366b5f82e15
Riwajchalise <wazzree@gmail.com>
2019-01-22 09:14:01 +0545

    First commit

---
+++ age.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Chalise on 1/22/2019.
+ */
+"use strict";
+
+var birth_year = 1998;
+var now_year = 2019;
+var age;
+
+age = now_year - birth_year;
+console.log("The age is", age, "or", age + 1);
